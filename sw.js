f56fb774a2c241565ab6bf2e9bb539f1cc769c86
Jan Barlak <47349005+janbarlak@users.noreply.github.com>
2026-07-28 13:28:14 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,6 +2,10 @@ self.addEventListener('install', () => {
   self.skipWaiting();
 });
 
+self.addEventListener('activate', () => {
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(fetch(event.request));
 });
